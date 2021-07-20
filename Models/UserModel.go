6cf205f55afb46c87c52b5cfbb2423fb63882f9d
Puwarin Maneechansuk <puwarin.ma@carmana.com>
2021-07-20 12:42:50 +0700

    qw
    qw

---
+++ Models/UserModel.go
@@ -0,0 +1,11 @@
+package Models
+type User struct {
+ Id      uint   `json:"id"`
+ Name    string `json:"name"`
+ Age   string `json:"age"`
+ Position   string `json:"position"`
+ Wage string `json:"wage"`
+}
+func (b *User) TableName() string {
+ return "user"
+}
--- main.go
+++ main.go
@@ -1,15 +1,36 @@
+// package main
+
+// import (
+// 	"github.com/gin-gonic/gin"
+// 	// "net/http"
+// 	"github.com/puwarinnueng/gin-marketplace/controllers"
+// )
+
+// func main() {
+// 	r := gin.Default()
+// 	r.GET("/", controllers.Get)
+// 	r.GET("/data", controllers.Data)
+// 	r.Run(":8080")
+// }
+
+
 package main
-
 import (
-	"github.com/gin-gonic/gin"
-	// "net/http"
-	"github.com/puwarinnueng/gin-marketplace/controllers"
+ "gin-server/Config"
+ "gin-server/Models"
+ "gin-server/Routes"
+ "fmt"
+"github.com/jinzhu/gorm"
 )
-
+var err error
 func main() {
-	r := gin.Default()
-	r.GET("/", controllers.Get )
-	r.GET("/data", controllers.Data )
-	r.Run(":8080")
+ Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+if err != nil {
+  fmt.Println("Status:", err)
+ }
+defer Config.DB.Close()
+ Config.DB.AutoMigrate(&Models.User{})
+r := Routes.SetupRouter()
+ //running
+ r.Run()
 }
-
