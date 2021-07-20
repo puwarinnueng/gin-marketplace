e2a84149f26e0c76db854558f997001d67fd875d
Puwarin Maneechansuk <puwarin.ma@carmana.com>
2021-07-20 12:47:39 +0700

    11

--- main.go
+++ main.go
@@ -14,23 +14,27 @@
 // }
 
 
+
 package main
+
 import (
- "gin-server/Config"
- "gin-server/Models"
- "gin-server/Routes"
- "fmt"
-"github.com/jinzhu/gorm"
+	"fmt"
+	"gin-server/Config"
+	"gin-server/Models"
+	"gin-server/Routes"
+	"github.com/jinzhu/gorm"
 )
+
 var err error
+
 func main() {
- Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
-if err != nil {
-  fmt.Println("Status:", err)
- }
-defer Config.DB.Close()
- Config.DB.AutoMigrate(&Models.User{})
-r := Routes.SetupRouter()
- //running
- r.Run()
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
 }
